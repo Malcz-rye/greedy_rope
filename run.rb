81810fb9b0a61305fa5194771e62b55e88b6aad9
realtradam <mr.malczewski@gmail.com>
2021-03-10 17:19:19 -0500

    .

--- run.rb
+++ run.rb
@@ -1,18 +1,27 @@
-@all_ropes = []
-@cost = 0
+@cost_biggest = 0
+@cost_smallest = 0
 
-@all_ropes = ARGV.map(&:to_i)
+@all_ropes_biggest = ARGV.map(&:to_i)
+@all_ropes_smallest = ARGV.map(&:to_i)
 
-#ARGV.each do |a|
-#  @all_ropes << a
-#end
-
-while @all_ropes.length > 1
-  rope_one = @all_ropes.delete_at(@all_ropes.index(@all_ropes.max))
-  rope_two = @all_ropes.delete_at(@all_ropes.index(@all_ropes.max))
-  @cost += rope_one + rope_two + 1
-  @all_ropes << rope_one + rope_two
+while @all_ropes_biggest.length > 1
+  rope_one = @all_ropes_biggest.delete_at(@all_ropes_biggest.index(@all_ropes_biggest.max))
+  rope_two = @all_ropes_biggest.delete_at(@all_ropes_biggest.index(@all_ropes_biggest.max))
+  @cost_biggest += rope_one + rope_two + 1
+  @all_ropes_biggest << rope_one + rope_two
 end
 
-puts "Rope Size: #{@all_ropes}"
-puts "Cost: #{@cost}"
+while @all_ropes_smallest.length > 1
+  rope_one = @all_ropes_smallest.delete_at(@all_ropes_smallest.index(@all_ropes_smallest.min))
+  rope_two = @all_ropes_smallest.delete_at(@all_ropes_smallest.index(@all_ropes_smallest.min))
+  @cost_smallest += rope_one + rope_two + 1
+  @all_ropes_smallest << rope_one + rope_two
+end
+
+puts 'Prioritize Biggest Ropes:'
+puts "Rope Size: #{@all_ropes_biggest}"
+puts "Cost: #{@cost_biggest}"
+puts
+puts 'Prioritize Smallest Ropes:'
+puts "Rope Size: #{@all_ropes_smallest}"
+puts "Cost: #{@cost_smallest}"
